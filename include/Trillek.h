bc53ecef2101052210e346a126f13d483ce8d059
catageek <github@catageeks.tk>
2014-04-15 07:09:44 +0200

    Fix format and remove network specific lines

--- include/Trillek.h
+++ include/Trillek.h
@@ -5,8 +5,7 @@
 
 // make_unique will be in C++14. Implemented here since we're using C++11.
 template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args)
-{
+std::unique_ptr<T> make_unique(Args&&... args) {
 	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
@@ -14,14 +13,6 @@ namespace Trillek {
 
 	// type of an entity #id
 	typedef uint32_t id_t;
-
-	namespace reflection {
-		// Template methods that can be used for reflection.
-
-		// Method to get the name of the handler of packet <MAJOR,MINOR>
-		template <int MAJOR,int MINOR> const char* GetNetworkHandler(void) {}
-	}
-}
 
 }
 #endif // TRILLEK_H_INCLUDED
