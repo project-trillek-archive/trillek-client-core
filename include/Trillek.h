7ea0bbd124175efa78c6bb809e45a1b4ec062c88
catageek <github@catageeks.tk>
2014-04-15 21:24:48 +0200

    Fix indents and namespace typo

--- include/Trillek.h
+++ include/Trillek.h
@@ -6,13 +6,13 @@
 // make_unique will be in C++14. Implemented here since we're using C++11.
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args) {
-	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
-namespace Trillek {
+namespace trillek {
 
-	// type of an entity #id
-	typedef uint32_t id_t;
+    // type of an entity #id
+    typedef uint32_t id_t;
 
 }
 #endif // TRILLEK_H_INCLUDED
