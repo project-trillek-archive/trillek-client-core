6dd57462f3e08bef7aecfeed08c8b5a892e334ed
catageek <github@catageeks.tk>
2014-04-21 11:02:03 +0200

    Add preprocessor variable for build target

--- main/main.cpp
+++ main/main.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+
 int main(int argCount, char **argValues) {
+    #ifdef STANDALONE
+    std::cout << "Starting Trillek with standalone build..." << std::endl;
+    #endif // STANDALONE
 	return 0;
 }
