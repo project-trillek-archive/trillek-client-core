95e505ab00ce9e1c16b20d8281275bc47b20497e
catageek <github@catageeks.tk>
2014-04-24 19:11:41 +0200

    Remove standalone binary

--- main/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argCount, char **argValues) {
-    #ifdef STANDALONE
-    std::cout << "Starting Trillek with standalone build..." << std::endl;
-    #endif // STANDALONE
-	return 0;
-}
