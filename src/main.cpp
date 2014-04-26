d54055426431c46ece09f7248b0a3aff230146ca
catageek <github@catageeks.tk>
2014-04-26 22:53:21 +0200

    Add TrillekAllocator

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include <cstddef>
+
+size_t gAllocatedSize = 0;
+
 int main(int argCount, char **argValues) {
 	return 0;
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,8 @@
 #include "tests/AtomicQueueTest.h"
 #include "tests/AtomicMapTest.h"
 
+size_t gAllocatedSize = 0;
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	int ret = RUN_ALL_TESTS();
