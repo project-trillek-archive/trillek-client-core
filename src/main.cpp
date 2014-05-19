3d342e7ffc74a7f9d737ca7c0a0ca35ce7b9dba3
Adam Martin <adam.madram@gmail.com>
2014-05-18 23:25:54 -0500

    Main.cpp now loads a sample JSON file that does nothing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include "OS.h"
+#include "systems/json-parser.hpp"
 
 int main(int argCount, char **argValues) {
     trillek::OS os;
     os.InitializeWindow(800, 600, "Trillek Client Core", 3, 0);
+
+    trillek::system::JSONParser jparser;
+    jparser.Parse("assets/tests/sample.json");
 
     while (!os.Closing()) {
         os.OSMessageLoop();
     }
 
     os.Terminate();
-	return 0;
+
+    return 0;
 }
