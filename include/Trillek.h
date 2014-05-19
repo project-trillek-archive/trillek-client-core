cf5eb27a1e82535bc380b4d11341ce14cb2591fc
Adam Martin <adam.madram@gmail.com>
2014-05-18 21:53:37 -0500

    Style fixes
    Still need to rename some files to use lower-case names.

--- include/Trillek.h
+++ include/Trillek.h
@@ -3,24 +3,19 @@
 
 #include <stdint.h>
 
-// make_unique will be in C++14. Implemented here since we're using C++11.
-// Commented because even VS2013 does not support var args
-/*
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
-*/
 namespace trillek {
 
-    // type of an entity #id
-    typedef uint32_t id_t;
+// type of an entity #id
+typedef uint32_t id_t;
 
-    namespace reflection {
-        // Template methods that can be used for reflection.
-        // To use them just call GetTypeName<MyType>() to retrieve the name for the specified type.
-        template <typename TYPE> const char* GetTypeName(void) { }
-        template <typename TYPE> const unsigned int GetTypeID(void) { }
-    }
-}
+namespace reflection {
+
+// Template methods that can be used for reflection.
+// To use them just call GetTypeName<MyType>() to retrieve the name for the specified type.
+template <typename TYPE> const char* GetTypeName(void) { }
+template <typename TYPE> const unsigned int GetTypeID(void) { }
+
+} // End of reflection
+} // End fo trillek
+
 #endif // TRILLEK_H_INCLUDED
--- include/resources/TextFile.h
+++ include/resources/TextFile.h
@@ -5,75 +5,79 @@
 #include "systems/ResourceSystem.h"
 
 namespace trillek {
-    namespace resource {
-        class TextFile : public ResourceBase {
-        public:
-            TextFile() { }
-            ~TextFile() { }
+namespace resource {
 
-            /**
-             * \brief Returns a resource with the specified name.
-             *
-             * The only used initialization property is "filename".
-             * \param[in] const std::vector<Property> &properties The creation properties for the resource.
-             * \return bool True if initialization finished with no errors.
-             */
-            virtual bool Initialize(const std::vector<Property> &properties) {
-                for (const Property& p : properties) {
-                    std::string name = p.GetName();
-                    if (name == "filename") {
-                        this->fname = p.Get<std::string>();
-                    }
-                }
+class TextFile : public ResourceBase {
+public:
+    TextFile() { }
+    ~TextFile() { }
 
-                std::ifstream f(this->fname, std::ios::in);
-
-                if (f.is_open()) {
-                    f.seekg(0, std::ios::end);
-                    this->fsize = f.tellg();
-                    f.seekg(0);
-
-                    this->filebuf.resize(this->fsize);
-
-                    f.read(&this->filebuf[0], this->filebuf.size());
-
-                    f.close();
-
-                    return true;
-                }
-
-                return false;
+    /**
+    * \brief Returns a resource with the specified name.
+    *
+    * The only used initialization property is "filename".
+    * \param[in] const std::vector<Property> &properties The creation properties for the resource.
+    * \return bool True if initialization finished with no errors.
+    */
+    virtual bool Initialize(const std::vector<Property> &properties) {
+        for (const Property& p : properties) {
+            std::string name = p.GetName();
+            if (name == "filename") {
+                this->filename = p.Get<std::string>();
             }
+        }
 
-            /**
-            * \brief Appends text to the internal buffer.
-            *
-            * \param[in] const std::string &text The text to ammend to the internal buffer.
-            * \return
-            */
-            void AppendText(const std::string& text) {
-                this->filebuf += text;
-            }
+        std::ifstream f(this->filename, std::ios::in);
 
-            /**
-            * \brief Gets a reference to the internal buffer.
-            *
-            * \return const std::string& The reference to the internal buffer.
-            */
-            const std::string& GetText() {
-                return this->filebuf;
-            }
-        private:
-            std::string filebuf; // Contents of the text file
-            std::string fname; // Relative filename
-            std::streamsize fsize; // Size of the file in characters
-        };
+        if (f.is_open()) {
+            f.seekg(0, std::ios::end);
+            this->file_size = f.tellg();
+            f.seekg(0);
+
+            this->file_buffer.resize(this->file_size);
+
+            f.read(&this->file_buffer[0], this->file_buffer.size());
+
+            f.close();
+
+            return true;
+        }
+
+        return false;
     }
 
-    namespace reflection {
-        template <> inline const char* GetTypeName<resource::TextFile>() { return "Text"; }
-        template <> inline const unsigned int GetTypeID<resource::TextFile>() { return 1000; }
+    /**
+    * \brief Appends text to the internal buffer.
+    *
+    * \param[in] const std::string &text The text to ammend to the internal buffer.
+    * \return
+    */
+    void AppendText(const std::string& text) {
+        this->file_buffer += text;
     }
-}
+
+    /**
+    * \brief Gets a reference to the internal buffer.
+    *
+    * \return const std::string& The reference to the internal buffer.
+    */
+    const std::string& GetText() {
+        return this->file_buffer;
+    }
+private:
+    std::string file_buffer; // Contents of the text file
+    std::string filename; // Relative filename
+    std::streamsize file_size; // Size of the file in characters
+};
+
+} // End of resource
+
+namespace reflection {
+
+template <> inline const char* GetTypeName<resource::TextFile>() { return "Text"; }
+template <> inline const unsigned int GetTypeID<resource::TextFile>() { return 1000; }
+
+} // End of reflection
+} // End of trillek
 
 #endif
