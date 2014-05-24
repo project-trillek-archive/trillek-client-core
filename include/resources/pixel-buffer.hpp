b0564909bc62d5747d7f383564e1cff3956fdbdf
Meisaka Yukara <Meisaka.Yukara@gmail.com>
2014-05-24 04:31:47 +0000

    Filled out the pixel buffer API some more

--- include/resources/pixel-buffer.hpp
+++ include/resources/pixel-buffer.hpp
@@ -13,7 +13,6 @@ enum class ImageColorMode : uint32_t {
     UNKNOWN_MODE = 0xffffffffu,
     MONOCHROME = 0,
     MONOCHROME_A = 4,
-    INDEXED = 3,
     COLOR_RGB = 2,
     COLOR_RGBA = 6,
 };
@@ -33,18 +32,47 @@ public:
     bool Initialize(const std::vector<Property> &properties);
 
     PixelBuffer();
-    PixelBuffer(const PixelBuffer &);
+    virtual ~PixelBuffer();
+
+    PixelBuffer(const PixelBuffer &) = delete;
+    PixelBuffer & operator=(const PixelBuffer &) = delete;
+
     PixelBuffer(PixelBuffer &&);
-    PixelBuffer(uint32_t width, uint32_t height, uint32_t bitdepth, ImageColorMode mode);
-    /** \brief Create a copy of a pixel buffer in a different format.
-     */
-    PixelBuffer(const PixelBuffer &, uint32_t bitdepth, ImageColorMode mode);
-    PixelBuffer & operator=(const PixelBuffer &);
     PixelBuffer & operator=(PixelBuffer &&);
 
-    bool Create(uint32_t width, uint32_t height, uint32_t bitdepth, ImageColorMode mode);
+    /** \brief Construct a new pixel buffer.
+     * \param width the width of the buffer in pixels
+     * \param height the height of the buffer in pixels
+     * \param bitspersample the number of bits in a single channel, usually 8
+     * \param mode greyscale (Monochrome) or color (RGB), and whether there is alpha or no alpha
+     */
+    PixelBuffer(uint32_t width, uint32_t height, uint32_t bitspersample, ImageColorMode mode);
 
-    bool IsDirty();
+    /** \brief Create a copy of a pixel buffer.
+     * \param pbuf the pixel buffer to copy from
+     */
+    bool CreateCopy(const PixelBuffer & pbuf);
+
+    /** \brief Create a copy of a pixel buffer in a different format.
+     * \param pbuf the pixel buffer to copy from
+     * \param bitspersample the number of bits in a single channel, usually 8
+     * \param mode greyscale (Monochrome) or color (RGB), and whether there is alpha or no alpha
+     */
+    bool CreateCopy(const PixelBuffer & pbuf, uint32_t bitspersample, ImageColorMode mode);
+
+    /** \brief Create a new pixel buffer, replacing the old buffer (if any).
+     * \param width the width of the buffer in pixels
+     * \param height the height of the buffer in pixels
+     * \param bitspersample the number of bits in a single channel, usually 8
+     * \param mode greyscale (Monochrome) or color (RGB), and whether there is alpha or no alpha
+     */
+    bool Create(uint32_t width, uint32_t height, uint32_t bitspersample, ImageColorMode mode);
+
+    bool IsDirty() const;
+    /** \brief Mark dirty */
+    void Invalidate();
+    /** \brief Mark not dirty */
+    void Validate();
 
     /**
      * \brief Returns a pointer to the base address of image data for reading.
@@ -64,16 +92,21 @@ public:
      */
     void UnlockWrite();
 
-private:
-    uint32_t imagewidth;
-    uint32_t imageheight;
-    uint32_t bufferpitch; /// number of bytes to advance down 1 raster line
-    uint32_t imagebitdepth;
-    ImageColorMode imagemode;
+    uint32_t GetWidth() const { return imagewidth; }
+    uint32_t GetHeight() const { return imageheight; }
+    uint32_t GetPitch() const { return bufferpitch; }
 
     // meta data, used by some formats such as cursors
     uint32_t image_x;
     uint32_t image_y;
+
+protected:
+    uint32_t imagewidth;
+    uint32_t imageheight;
+    uint32_t bufferpitch; /// number of bytes to move vertical 1 raster line
+    uint32_t imagepixelsize;
+    uint32_t imagebitdepth;
+    ImageColorMode imagemode;
 
     bool dirty;
 
--- src/resources/pixel-buffer.cpp
+++ src/resources/pixel-buffer.cpp
@@ -8,9 +8,108 @@ namespace resource {
 
 PixelBuffer::PixelBuffer() :
     imagewidth(0), imageheight(0), bufferpitch(0), imagebitdepth(0),
-    imagemode(ImageColorMode::MONOCHROME),
+    imagemode(ImageColorMode::MONOCHROME), imagepixelsize(0),
     image_x(0), image_y(0), dirty(false)
 {
+}
+
+PixelBuffer::~PixelBuffer() {
+
+}
+
+PixelBuffer::PixelBuffer(PixelBuffer && rv) {
+    writelock.lock();
+    rv.writelock.lock();
+    imagewidth = rv.imagewidth;
+    imageheight = rv.imageheight;
+    bufferpitch = rv.bufferpitch;
+    imagebitdepth = rv.imagebitdepth;
+    imagepixelsize = rv.imagepixelsize;
+    imagemode = rv.imagemode;
+    image_x = rv.image_x;
+    image_y = rv.image_y;
+    dirty = rv.dirty;
+    blockptr = std::move(rv.blockptr);
+    rv.imageheight = 0;
+    rv.imagewidth = 0;
+    rv.bufferpitch = 0;
+    rv.writelock.unlock();
+    writelock.unlock();
+}
+PixelBuffer::PixelBuffer(uint32_t width, uint32_t height, uint32_t bitspersample, ImageColorMode mode) {
+    imagepixelsize = 0;
+    imagewidth = 0;
+    imageheight = 0;
+    imagebitdepth = 0;
+    imagemode = 0;
+    image_x = 0;
+    image_y = 0;
+    dirty = false;
+    Create(width, height, bitspersample, mode);
+}
+
+PixelBuffer & PixelBuffer::operator=(PixelBuffer && rv) {
+    writelock.lock();
+    rv.writelock.lock();
+    imagewidth = rv.imagewidth;
+    imageheight = rv.imageheight;
+    bufferpitch = rv.bufferpitch;
+    imagebitdepth = rv.imagebitdepth;
+    imagepixelsize = rv.imagepixelsize;
+    imagemode = rv.imagemode;
+    image_x = rv.image_x;
+    image_y = rv.image_y;
+    dirty = rv.dirty;
+    blockptr = std::move(rv.blockptr);
+    rv.imageheight = 0;
+    rv.imagewidth = 0;
+    rv.bufferpitch = 0;
+    rv.writelock.unlock();
+    writelock.unlock();
+    return *this;
+}
+
+bool PixelBuffer::Create(uint32_t width, uint32_t height, uint32_t bitspersample, ImageColorMode mode) {
+    uint8_t pixelsize;
+    switch(mode) {
+    case ImageColorMode::COLOR_RGB:
+        pixelsize = 3;
+        break;
+    case ImageColorMode::COLOR_RGBA:
+        pixelsize = 4;
+        break;
+    case ImageColorMode::MONOCHROME:
+        pixelsize = 1;
+        break;
+    case ImageColorMode::MONOCHROME_A:
+        pixelsize = 2;
+        break;
+    default:
+        pixelsize = 0;
+        break;
+    }
+    imagepixelsize = pixelsize;
+    imagewidth = width;
+    imageheight = height;
+    imagebitdepth = bitspersample;
+    imagemode = mode;
+    image_x = 0;
+    image_y = 0;
+    dirty = true;
+    bufferpitch = (width * bitspersample * imagepixelsize);
+    bufferpitch = (bufferpitch >> 3) + ((bufferpitch & 0x7) ? 1 : 0);
+    blockptr = std::unique_ptr<uint8_t[]>(new uint8_t[bufferpitch * height]);
+    return true;
+}
+
+bool PixelBuffer::IsDirty() {
+    return dirty;
+}
+void PixelBuffer::Invalidate() {
+    dirty = true;
+}
+void PixelBuffer::Validate() {
+    dirty = false;
 }
 
 bool PixelBuffer::Initialize(const std::vector<Property> &properties) {
@@ -36,4 +135,4 @@ bool PixelBuffer::Initialize(const std::vector<Property> &properties) {
 }
 
 }
-}
+} // trillek
