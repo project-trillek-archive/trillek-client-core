0835d2be009d3402fdbf9844e635286af7bc4ea9
Meisaka Yukara <Meisaka.Yukara@gmail.com>
2014-06-13 06:42:32 +0000

    spelling and indent on more files

--- include/components/renderable.hpp
+++ include/components/renderable.hpp
@@ -38,59 +38,59 @@ public:
     };
 
     /**
-    * \brief Updates (or creates) the Renderable's BufferGroups.
-    *
-    * This will take the buffers from the mesh resource mesh groups and put
-    * them in OpenGL buffers. This will create new buffer groups if needed.
-    * \return void
-    */
+     * \brief Updates (or creates) the Renderable's BufferGroups.
+     *
+     * This will take the buffers from the mesh resource mesh groups and put
+     * them in OpenGL buffers. This will create new buffer groups if needed.
+     * \return void
+     */
     void UpdateBufferGroups();
 
     /**
-    * \brief Sets the mesh resource associated with this component.
-    *
-    * \param std::shared_ptr<resource::Mesh> m The mesh reousrce for this component.
-    * \return void
-    */
+     * \brief Sets the mesh resource associated with this component.
+     *
+     * \param std::shared_ptr<resource::Mesh> m The mesh resource for this component.
+     * \return void
+     */
     void SetMesh(std::shared_ptr<resource::Mesh> m);
 
     /**
-    * \brief Gets the mesh resource associated with this component.
-    *
-    * \return std::shared_ptr<resource::Mesh> The mesh reousrce for this component.
-    */
+     * \brief Gets the mesh resource associated with this component.
+     *
+     * \return std::shared_ptr<resource::Mesh> The mesh resource for this component.
+     */
     std::shared_ptr<resource::Mesh> GetMesh() const;
 
     /**
-    * \brief Sets the shader resource associated with this component.
-    *
-    * \param std::shared_ptr<Material> m The shader for this component.
-    * \return void
-    */
+     * \brief Sets the shader resource associated with this component.
+     *
+     * \param std::shared_ptr<Material> m The shader for this component.
+     * \return void
+     */
     void SetShader(std::shared_ptr<Shader> s);
 
     /**
-    * \brief Gets the shader for this renderable
-    *
-    * \return std::shared_ptr<Material> This shader for this component.
-    */
+     * \brief Gets the shader for this renderable
+     *
+     * \return std::shared_ptr<Material> This shader for this component.
+     */
     std::shared_ptr<Shader> GetShader() const;
 
     /**
-    * \brief Gets the number of buffer groups.
-    *
-    * \return size_t The number of buffer groups.
-    */
+     * \brief Gets the number of buffer groups.
+     *
+     * \return size_t The number of buffer groups.
+     */
     size_t GetBufferGroupCount() const {
         return this->buffer_groups.size();
     }
 
     /**
-    * \brief Gets the specific buffer group.
-    *
-    * \param const size_t index The index of the buffer group to return.
-    * \return std::shared_ptr<BufferGroup> The requested buffer group.
-    */
+     * \brief Gets the specific buffer group.
+     *
+     * \param const size_t index The index of the buffer group to return.
+     * \return std::shared_ptr<BufferGroup> The requested buffer group.
+     */
     std::shared_ptr<BufferGroup> GetBufferGroup(const size_t index) const {
         if (index < this->buffer_groups.size()) {
             return this->buffer_groups[index];
@@ -100,12 +100,12 @@ public:
     }
 
     /**
-    * \brief Initializes the component with the provided properties
-    *
-    * Valid properties include mesh (the mesh resource name) and shader (the shader resource name).
-    * \param[in] const std::vector<Property>& properties The creation properties for the component.
-    * \return bool True if initialization finished with no errors.
-    */
+     * \brief Initializes the component with the provided properties
+     *
+     * Valid properties include mesh (the mesh resource name) and shader (the shader resource name).
+     * \param[in] const std::vector<Property>& properties The creation properties for the component.
+     * \return bool True if initialization finished with no errors.
+     */
     bool Initialize(const std::vector<Property> &properties);
 private:
     std::vector<std::shared_ptr<BufferGroup>> buffer_groups; // Render buffer ID group
