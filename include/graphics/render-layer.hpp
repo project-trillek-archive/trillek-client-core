2a794e87724bb3199027e42e75fa8d52fc7ca586
Meisaka Yukara <Meisaka.Yukara@gmail.com>
2014-06-17 04:38:11 +0000

    Started adding classes for multipass rendering
    The first two are for FBOs and render targets. Render targets can be
    textures, or textures and renderbuffers with multisampling.

---
+++ include/graphics/render-layer.hpp
@@ -0,0 +1,66 @@
+#ifndef RENDER_LAYER_HPP_INCLUDED
+#define RENDER_LAYER_HPP_INCLUDED
+
+#include "opengl.hpp"
+#include "trillek.hpp"
+#include "graphics-base.hpp"
+#include <rapidjson/document.h>
+#include "texture.hpp"
+#include <vector>
+#include <memory>
+
+namespace trillek {
+namespace graphics {
+
+/**
+ * \brief A class to manage a color or depth output from a render pass
+ */
+class RenderAttachment : public GraphicsBase {
+public:
+    RenderAttachment();
+    ~RenderAttachment();
+    RenderAttachment(const RenderAttachment &) = delete;
+    RenderAttachment& operator=(const RenderAttachment &) = delete;
+    RenderAttachment(RenderAttachment &&that);
+    RenderAttachment& operator=(RenderAttachment &&that);
+
+private:
+    GLuint renderbuf;
+    bool multisample;
+    GLenum attachtarget;
+    std::string texturename;
+    std::weak_ptr<Texture> texture;
+};
+
+/**
+ * \brief A class to handle rendering a pass to a framebuffer
+ */
+class RenderLayer : public GraphicsBase {
+public:
+    RenderLayer();
+    virtual ~RenderLayer();
+
+    /**
+     * \brief parse a RenderLayer from json
+     * \return false on errors, true for success
+     */
+    bool Parse(rapidjson::Value& node);
+private:
+
+    GLuint fbo_id;
+    std::vector<std::weak_ptr<RenderAttachment>> attachments;
+};
+
+} // namespace graphics
+
+namespace reflection {
+
+template <> inline const char* GetTypeName<graphics::RenderAttachment>() { return "attachment"; }
+template <> inline const unsigned int GetTypeID<graphics::RenderAttachment>() { return 402; }
+template <> inline const char* GetTypeName<graphics::RenderLayer>() { return "render"; }
+template <> inline const unsigned int GetTypeID<graphics::RenderLayer>() { return 403; }
+
+} // namespace reflection
+} // namespace trillek
+
+#endif
---
+++ src/graphics/render-layer.cpp
@@ -0,0 +1,49 @@
+
+#include "graphics/render-layer.hpp"
+
+namespace trillek {
+namespace graphics {
+
+RenderAttachment::RenderAttachment() {
+    renderbuf = 0;
+    this->attachtarget = GL_COLOR_ATTACHMENT0;
+    this->multisample = false;
+}
+
+RenderAttachment::~RenderAttachment() {
+
+}
+
+RenderAttachment::RenderAttachment(RenderAttachment &&that) {
+    this->renderbuf = that.renderbuf;
+    this->texturename = std::move(that.texturename);
+    this->attachtarget = that.attachtarget;
+    this->multisample = that.multisample;
+    that.renderbuf = 0;
+    this->texture = std::move(that.texture);
+}
+
+RenderAttachment& RenderAttachment::operator=(RenderAttachment &&that) {
+    this->renderbuf = that.renderbuf;
+    this->texturename = std::move(that.texturename);
+    this->attachtarget = that.attachtarget;
+    this->multisample = that.multisample;
+    that.renderbuf = 0;
+    this->texture = std::move(that.texture);
+    return *this;
+}
+
+RenderLayer::RenderLayer() {
+    fbo_id = 0;
+}
+
+RenderLayer::~RenderLayer() {
+
+}
+
+bool RenderLayer::Parse(rapidjson::Value& node) {
+    return true;
+}
+
+} // namespace graphics
+} // namespace trillek
