1c83eebaaf68fc6b01a202ffa9bc0cdb86953cae
catageek <github@catageeks.tk>
2014-08-28 22:04:27 +0200

    Add RakNet library as dependency

--- build.ps1
+++ build.ps1
@@ -1,4 +1,9 @@
 pushd
+cd raknet
+cmake -G "Visual Studio 12" -D CMAKE_BUILD_TYPE:String=Release .
+msbuild ALL_BUILD.vcxproj /p:Configuration=Release /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+popd
+pushd
 cd crypto++
 cmake -G "Visual Studio 12" -D CMAKE_BUILD_TYPE:String=Release .
 msbuild ALL_BUILD.vcxproj /p:Configuration=Release /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
