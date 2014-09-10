7cf8b56aa2587de1a69425d6e853feb61a5b3ad6
catageek <github@catageeks.tk>
2014-09-10 22:27:43 +0200

    Add Bullet to appveyor script

--- prebuild.ps1
+++ prebuild.ps1
@@ -2,7 +2,7 @@
 
 new-item lib -type directory
 new-item lib\x86 -type directory
-new-item lib\x86\Debug -type directory
+new-item lib\x86\Release -type directory
 new-item lib\include -type directory
 new-item build -type directory
 
@@ -31,6 +31,19 @@ Start-FileDownload 'http://heanet.dl.sourceforge.net/project/ogl-math/glm-0.9.5.
 7z -y x glm-0.9.5.3.7z
 move-item -path glm\* -destination lib/include
 
+# Get Bullet
+Start-FileDownload 'https://bullet.googlecode.com/files/bullet-2.82-r2704.zip'
+7z -y x bullet-2.82-r2704.zip
+pushd
+Set-Location bullet-2.82-r2704
+cmake -G "Visual Studio 12" -D CMAKE_BUILD_TYPE:String=Release -DINSTALL_LIBS=on -DBUILD_SHARED_LIBS=on -DBUILD_DEMOS=off -DUSE_DOUBLE_PRECISION=on  .
+msbuild BULLET_PHYSICS.sln /p:Configuration=Release /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+Get-ChildItem lib/Release/*
+popd
+move-item -path bullet-2.82-r2704\lib\Release\include\* -destination lib/include
+move-item -path bullet-2.82-r2704\lib\Release\*.lib -destination lib/x86/Release
+move-item -path bullet-2.82-r2704\lib\Release\*.dll -destination lib/x86/Release
+
 # Get and build GTest
 Start-FileDownload 'http://googletest.googlecode.com/files/gtest-1.7.0.zip'
 7z -y x gtest-1.7.0.zip
@@ -40,6 +53,6 @@ cmake -G "Visual Studio 12" -D CMAKE_BUILD_TYPE:String=Release .
 msbuild gtest.sln /p:Configuration=Release /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 popd
 move-item -path gtest-1.7.0\include\* -destination lib/include
-move-item -path gtest-1.7.0\Release\*.lib -destination lib/x86/Debug
-move-item -path gtest-1.7.0\Release\*.dll -destination lib/x86/Debug
+move-item -path gtest-1.7.0\Release\*.lib -destination lib/x86/Release
+move-item -path gtest-1.7.0\Release\*.dll -destination lib/x86/Release
 Get-ChildItem lib/x86/Release/*
