e946a01b530f67b8a9f7b1e3fa4ac12dbfba1907
catageek <github@catageeks.tk>
2014-10-10 23:42:00 +0200

    Fix signature of Initialize() for ReferenceFrame component

--- include/components/component-templates.hpp
+++ include/components/component-templates.hpp
@@ -11,7 +11,7 @@ namespace component {
 
 extern template std::shared_ptr<Container> Initialize<Component::VelocityMax>(const std::vector<Property> &properties);
 extern template std::shared_ptr<Container> Initialize<Component::Collidable>(const std::vector<Property> &properties);
-extern template std::shared_ptr<Container> Initialize<Component::ReferenceFrame>(const std::vector<Property> &properties);
+extern template id_t Initialize<Component::ReferenceFrame>(bool& result, const std::vector<Property> &properties);
 
 } // namespace component
 } // namespace trillek
